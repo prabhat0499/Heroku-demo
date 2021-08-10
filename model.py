b8b0ce5591ed80073ac25bcf641f323e63da7b4a
Prabhat Dangi <83132841+prabhat0499@users.noreply.github.com>
2021-08-10 23:43:35 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 21:10:58 2021
+
+@author: Prabhat Dangi
+"""
+
+import pandas as pd
+import numpy as np
+import pickle
+
+df=pd.read_csv("F:\Documents\Salary.csv")
+
+print(df)
+
+X=df.drop(['Salary'],axis=1)
+y=df['Salary']
+
+from sklearn.linear_model import LinearRegression
+
+lg=LinearRegression()
+
+lg.fit(X,y)
+
+pickle.dump(lg, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[6, 76]]))
